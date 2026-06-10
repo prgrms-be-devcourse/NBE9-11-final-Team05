6aff047ca10ef0bfd0385c7da64118624c27ab87
BroJin <gudwls2777@naver.com>
2026-06-11 01:51:12 +0900

    backend init

---
+++ backend/ovengers/src/main/java/com/back/ovengers/OvengersApplication.java
@@ -0,0 +1,13 @@
+package com.back.ovengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvengersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvengersApplication.class, args);
+    }
+
+}
---
+++ backend/ovengers/src/test/java/com/back/ovengers/OvengersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.ovengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvengersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
