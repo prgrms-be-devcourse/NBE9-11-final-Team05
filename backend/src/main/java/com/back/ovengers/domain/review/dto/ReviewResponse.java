b52f5b5dfebc5f456c8100e4ebb8f1e597f666dd
alison0525 <ak501636@gmail.com>
2026-06-15 15:05:11 +0900

    feat: ReviewResponse 생성

--- backend/src/main/java/com/back/ovengers/domain/review/dto/ReviewResponse.java
+++ backend/src/main/java/com/back/ovengers/domain/review/dto/ReviewResponse.java
@@ -1,4 +1,30 @@
 package com.back.ovengers.domain.review.dto;
 
+import com.back.ovengers.domain.review.entity.Review;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class ReviewResponse {
+
+    private Long reviewId;
+    private String writerNickname;
+    private String writerImageUrl;
+    private Integer rating;
+    private String content;
+    private LocalDateTime createdAt;
+
+    private ReviewResponse(Review review) {
+        this.reviewId = review.getId();
+        this.writerNickname = review.getUser().getNickname();
+        this.writerImageUrl = review.getUser().getImageUrl();
+        this.rating = review.getRating();
+        this.content = review.getContent();
+        this.createdAt = review.getCreatedAt();
+    }
+
+    public static ReviewResponse from(Review review){
+        return new ReviewResponse(review);
+    }
 }
