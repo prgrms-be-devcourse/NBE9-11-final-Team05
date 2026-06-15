79f2a828240be05243dc81ff271aaac309cba55b
alison0525 <ak501636@gmail.com>
2026-06-15 12:41:56 +0900

    feat: ReviewController 추가

--- backend/src/main/java/com/back/ovengers/domain/review/controller/ReviewController.java
+++ backend/src/main/java/com/back/ovengers/domain/review/controller/ReviewController.java
@@ -1,8 +1,29 @@
 package com.back.ovengers.domain.review.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.back.ovengers.domain.review.dto.ReviewRequest;
+import com.back.ovengers.domain.review.service.ReviewService;
+import com.back.ovengers.domain.user.entity.User;
+import com.back.ovengers.global.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/reviews")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> createReview(
+            @AuthenticationPrincipal User user,
+            @RequestParam Long reservationId,
+            @Valid @RequestBody ReviewRequest request
+    ){
+        reviewService.createReview(user, reservationId, request);
+        return ResponseEntity.ok(new ApiResponse<>("리뷰가 작성되었습니다."));
+    }
 }
