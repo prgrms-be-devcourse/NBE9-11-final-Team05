5ba694a16ec002760ecda199a4ddd75d5d3ece4f
alison0525 <ak501636@gmail.com>
2026-06-15 10:47:09 +0900

    feat: ReviewRequest DTO 생성

--- backend/src/main/java/com/back/ovengers/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/back/ovengers/domain/review/repository/ReviewRepository.java
@@ -1,4 +1,7 @@
 package com.back.ovengers.domain.review.repository;
 
-public class ReviewRepository {
+import com.back.ovengers.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
