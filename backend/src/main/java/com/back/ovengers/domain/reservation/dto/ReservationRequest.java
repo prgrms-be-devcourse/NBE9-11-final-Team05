c7a84d9a3b5e2d326c143b89c2e629b8c59b8d4d
BroJin <gudwls2777@naver.com>
2026-06-15 10:16:49 +0900

    refactor: import 변경 및 Positive 추가

--- backend/src/main/java/com/back/ovengers/domain/reservation/dto/ReservationRequest.java
+++ backend/src/main/java/com/back/ovengers/domain/reservation/dto/ReservationRequest.java
@@ -1,8 +1,10 @@
 package com.back.ovengers.domain.reservation.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
-import org.antlr.v4.runtime.misc.NotNull;
+
 
 import java.time.LocalDate;
 
@@ -25,6 +27,7 @@ public class ReservationRequest {
     private LocalDate checkOut;
 
     @NotNull
+    @Positive
     private Integer guestCount;
 
     private String request;
--- backend/src/main/java/com/back/ovengers/domain/reservation/repository/ReservationRepository.java
+++ backend/src/main/java/com/back/ovengers/domain/reservation/repository/ReservationRepository.java
@@ -1,25 +1,20 @@
 package com.back.ovengers.domain.reservation.repository;
 
 import com.back.ovengers.domain.reservation.entity.Reservation;
-import com.back.ovengers.domain.site.entity.Site;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
 
     // 해당 날짜에 예약된 수 조회
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("""
     SELECT COUNT(r) FROM Reservation r
     WHERE r.site.id = :siteId
-    AND r.status != 'CANCELLED'
+    AND r.status != :cancelledStatus
     AND r.checkIn < :checkOut
     AND r.checkOut > :checkIn
 """)
