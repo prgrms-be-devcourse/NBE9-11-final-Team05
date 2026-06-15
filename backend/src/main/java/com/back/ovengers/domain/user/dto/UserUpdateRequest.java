ef96a1b2f70d97115c5406e8a9d702cb41ee8239
JunWoo Seo <csjws@naver.com>
2026-06-15 11:34:00 +0900

    refactor: @NoArgsConstructor 제거

--- backend/src/main/java/com/back/ovengers/domain/user/dto/UserUpdateRequest.java
+++ backend/src/main/java/com/back/ovengers/domain/user/dto/UserUpdateRequest.java
@@ -1,10 +1,8 @@
 package com.back.ovengers.domain.user.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class UserUpdateRequest {
     private String nickname;
 
