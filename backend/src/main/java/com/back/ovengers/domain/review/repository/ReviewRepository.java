61fd17008cbe7ecc3628485679f850d4f5feacf7
alison0525 <ak501636@gmail.com>
2026-06-15 11:09:55 +0900

    feat: ReviewRepo 생성 및 ErrorCode 추가

--- backend/src/main/java/com/back/ovengers/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/back/ovengers/domain/review/repository/ReviewRepository.java
@@ -3,5 +3,13 @@ package com.back.ovengers.domain.review.repository;
 import com.back.ovengers.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // 중복 리뷰 방지
+    boolean existsByReservationId(Long reservationId);
+
+    // 캠핑장 리뷰 목록 조회
+    List<Review> findByCampingId(Long campingId);
 }
