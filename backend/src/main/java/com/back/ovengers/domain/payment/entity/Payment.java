44b10e416630cb69fc3f2f4c1738a3f5f0a61563
BroJin <gudwls2777@naver.com>
2026-06-16 12:13:54 +0900

    feat: 결제 생성 구현

--- backend/src/main/java/com/back/ovengers/domain/payment/entity/Payment.java
+++ backend/src/main/java/com/back/ovengers/domain/payment/entity/Payment.java
@@ -1,4 +1,35 @@
 package com.back.ovengers.domain.payment.entity;
 
-public class Payment {
+import com.back.ovengers.domain.reservation.entity.Reservation;
+import com.back.ovengers.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "payment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class Payment extends BaseEntity {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id", nullable = false)
+    private Reservation reservation;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    private String paymentKey;
+
+    @Column(nullable = false)
+    private Integer paidPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    public void updateStatus(PaymentStatus status) {
+        this.status = status;
+    }
 }
---
+++ backend/src/main/java/com/back/ovengers/domain/payment/entity/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.back.ovengers.domain.payment.entity;
+
+public enum PaymentStatus {
+    READY, DONE, CANCELLED, FAILED
+}
