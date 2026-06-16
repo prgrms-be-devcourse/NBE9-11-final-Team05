7dcef5fbc5d78905ad5915ffc9d56574a775d7aa
alison0525 <ak501636@gmail.com>
2026-06-16 14:45:41 +0900

    chore: 사용하지 않는 import 제거

--- backend/src/main/java/com/back/ovengers/global/config/SecurityConfig.java
+++ backend/src/main/java/com/back/ovengers/global/config/SecurityConfig.java
@@ -18,8 +18,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-import static org.springframework.security.authorization.SingleResultAuthorizationManager.permitAll;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
