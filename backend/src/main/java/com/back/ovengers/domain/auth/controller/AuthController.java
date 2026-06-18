b55fe4955e8a7c421e3eabe1787873ab3fa4f00c
JunWoo Seo <csjws@naver.com>
2026-06-18 09:28:35 +0900

    refactor: 오타 수정

--- backend/src/main/java/com/back/ovengers/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/back/ovengers/domain/auth/controller/AuthController.java
@@ -95,6 +95,6 @@ public class AuthController {
     ){
         authService.checkNickname(nickname);
 
-        return ResponseEntity.ok(new ApiResponse<>("사용 가능한 이메일입니다"));
+        return ResponseEntity.ok(new ApiResponse<>("사용 가능한 닉네임입니다"));
     }
 }
